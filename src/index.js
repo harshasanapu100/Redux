58c05beb489b9dcd4f1989212ac2b4f5061b0043
harshasnapu100 <harshasnapu100@gmail.com>
2023-07-12 09:58:03 +0000

    Add action creators

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved } from "./actionCreators";
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(bugAdded("Bug1"));
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+store.dispatch(bugRemoved(1));
 
 console.log(store.getState());
 
