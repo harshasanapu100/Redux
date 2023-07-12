19567754a19882f59750ef2ad09470a2ce315fa3
harshasnapu100 <harshasnapu100@gmail.com>
2023-07-12 06:38:44 +0000

    Functional programming with different approaches

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("Hello World!");
+import { compose, pipe } from "lodash/fp";
+
+// Trim the input, convert it into uppercase and finally wrap in div
+let input = "   Javascript      ";
+
+// Non functional programming
+let output = "<div>" + input.trim().toUpperCase() + "</div>";
+
+// Functional programming
+const trim = (str) => str.trim();
+const convertToUppercase = (str) => str.toUpperCase();
+const wrapInDiv = (str) => `<div> ${str} </div>`;
+
+// Functional programming using basics
+output = wrapInDiv(convertToUppercase(trim(input)));
+console.log(output);
+
+// Functional programming using Compose from lodash library
+const transfromUsingCompose = compose(wrapInDiv, convertToUppercase, trim);
+output = transfromUsingCompose(input);
+console.log(output);
+
+// Functional programming using Pipe from lodash library
+const transfromUsingPipe = pipe(trim, convertToUppercase, wrapInDiv);
+output = transfromUsingPipe(input);
+console.log(output);
