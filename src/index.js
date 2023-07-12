84cfc6a00da8bfa7e1bc1e7b7f12b3850f8a1488
harshasnapu100 <harshasnapu100@gmail.com>
2023-07-12 10:30:11 +0000

    Update import statement of action creators

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import store from "./store";
-import { bugAdded, bugRemoved, bugResloved } from "./actionCreators";
+import * as actionCreators from "./actionCreators";
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed", store.getState());
 });
 
-store.dispatch(bugAdded("Bug1"));
+store.dispatch(actionCreators.bugAdded("Bug1"));
 
-store.dispatch(bugResloved(1));
+store.dispatch(actionCreators.bugResloved(1));
 
-store.dispatch(bugRemoved(1));
+store.dispatch(actionCreators.bugRemoved(1));
 
 console.log(store.getState());
 
