68bf5f83f586cba3407af21b2f71ee487ca378b9
harshasnapu100 <harshasnapu100@gmail.com>
2023-07-14 03:11:35 +0000

    Combine Actions and Reducers using createSlice produced by redux toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,42 +1,35 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 // The below pattern implemented using Ducks patteren. Points to remember in Ducks pattern
 // 1) Move Action types, Action creators and Reducer into this single module.
 // 1) Reducer has to be default export in this module.
 
-// Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
-
-// Action creators
-export const bugAdded = createAction(BUG_ADDED);
-
-export const bugRemoved = createAction(BUG_REMOVED);
-
-export const bugResloved = createAction(BUG_RESOLVED);
-
-// Reducer
-
 let lastId = 0;
-export default createReducer([], {
-  // key : value
-  // actions: functions to handle the action (event => event handler)
 
-  [BUG_ADDED]: (state, action) => {
-    state.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    // actions => actions handlers
 
-  [BUG_REMOVED]: (state, action) => {
-    const index = state.findIndex((bug) => bug.id == action.payload.id);
-    state.splice(index, 1);
-  },
+    bugAdded: (state, action) => {
+      state.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
 
-  [BUG_RESOLVED]: (state, action) => {
-    const index = state.findIndex((bug) => bug.id == action.payload.id);
-    state[index].resolved = true;
+    bugRemoved: (state, action) => {
+      const index = state.findIndex((bug) => bug.id == action.payload.id);
+      state.splice(index, 1);
+    },
+
+    bugResloved: (state, action) => {
+      const index = state.findIndex((bug) => bug.id == action.payload.id);
+      state[index].resolved = true;
+    },
   },
 });
+
+export const { bugAdded, bugRemoved, bugResloved } = slice.actions;
+export default slice.reducer;
