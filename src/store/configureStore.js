b259aded18024c7845a9a1a1ecb9d86452e92d00
harshasnapu100 <harshasnapu100@gmail.com>
2023-07-16 07:02:04 +0000

    Configure thunk middleware from reudx toolkit. Remove custom middleware from store configuration

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
-import func from "./middleware/func";
 
 export default function () {
   return configureStore({
     reducer: reducer,
-    middleware: [logger({ destionation: "console" }), func],
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({ destionation: "console" }),
+    ],
   });
 }
--- src/store/middleware/func.js
+++ src/store/middleware/func.js
@@ -1,3 +1,5 @@
+// This middleware is already built for us by redux toolkit. Its call thunk. If we use redux toolkit
+// we can ignore the below middleware. Just need to modify the store to make it work.
 const func =
   ({ dispatch, getState }) =>
   (next) =>
